153d32c7fa9e77b4e4721a2dcd17f5dce6053fbf
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2026-04-29 12:05:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 function toggleMenu(){
 document.getElementById("navLinks").classList.toggle("active");
 }
+
+/* 🔥 dropdown */
+function toggleDrop(id){
+let el = document.getElementById(id);
+
+if(el.style.display === "block"){
+el.style.display = "none";
+}else{
+el.style.display = "block";
+}
+}
+
+/* 🔥 table expand */
+function toggleRow(id){
+document.getElementById(id).classList.toggle("hidden");
+}
